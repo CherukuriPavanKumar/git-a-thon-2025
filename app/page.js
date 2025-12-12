9acb5a8055e6e61e0903fefcf0bc2d5bb8546482
Somanadh T <tummalasomanadh@gmail.com>
2025-12-12 22:56:23 +0530

    Fixed Hero section text clipping issue and added a working timer

--- app/page.js
+++ app/page.js
@@ -6,13 +6,9 @@ import ProbStmts from "../components/ProbStmts";
 export default function Home() {
   return (
     <main className="bg-[#0f1214] text-white min-h-screen">
-      {/* Navbar */}
       <Navbar />
-
-      {/* Hero Section */}
       <Hero />
 
-      {/* Other Sections with proper spacing */}
       <div className="px-4 md:px-20 space-y-28 mt-20">
         <About />
         <ProbStmts />
